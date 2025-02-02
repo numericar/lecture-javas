dd6a530d388ed173bd6164f5d4f13ba719cf4ca3
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-03 00:19:54 +0700

    lecture: data type

---
+++ codes/Lec004DataType.java
@@ -0,0 +1,8 @@
+package codes;
+
+public class Lec004DataType {
+    public static void main(String args[]) {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
